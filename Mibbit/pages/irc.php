47f00456ade1376e93cbf42e99b30dffcafb1852
John Reese <jreese@leetcode.net>
2009-02-03 15:03:55 -0500

    Implemented basic inline widget page.

---
+++ Mibbit/pages/irc.php
@@ -0,0 +1,42 @@
+<?php
+# Copyright (C) 2008	John Reese
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+
+access_ensure_global_level( plugin_config_get( 'view_threshold' ) );
+
+$t_mibbit_uri = mibbit_generate_uri();
+
+html_page_top1( plugin_lang_get( 'irc' ) );
+html_page_top2();
+?>
+
+<br/>
+<table class="width75" cellspacing="1" align="center">
+
+<tr>
+<td class="form-title"><?php echo plugin_lang_get( 'irc' ) ?></td>
+<td class="right"><?php echo mibbit_popup( plugin_lang_get( 'use_popup' ) ) ?></td>
+</tr>
+
+<tr>
+<td colspan="2">
+
+<?php echo mibbit_iframe() ?>
+
+</td>
+</tr>
+
+</table>
+
+<?php
+html_page_bottom1( __FILE__ );
+
